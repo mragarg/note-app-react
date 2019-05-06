63eee71885ff5dd52fbcc40e1b304b687669cc6e
Ashish Garg <mragarg@gmail.com>
2019-05-06 11:14:13 -0400

    Sends id to parent when clicked

--- src/NotesApp.js
+++ src/NotesApp.js
@@ -6,27 +6,28 @@ import styles from './NotesApp.module.css'
 export class NotesApp extends React.Component {
 
     constructor(props) {
-      super(props)
+        super(props)
     
-      this.state = {
-         notes: [   // This will hold an array of objects
-             {
-                 id: '1001',
-                 title: 'first note',
-                 text: 'this is the first note'
-             },
-             {
-                 id: '1002',
-                 title: 'second note',
-                 text: 'this is the second note'
-             },
-             {
-                 id: '1003',
-                 title: 'third note',
-                 text: 'this is the third note'
-             }
-         ]
-      }
+        this.state = {
+            selectedNote: '1001',
+            notes: [   // This will hold an array of objects
+                {
+                    id: '1001',
+                    title: 'first note',
+                    text: 'this is the first note'
+                },
+                {
+                    id: '1002',
+                    title: 'second note',
+                    text: 'this is the second note'
+                },
+                {
+                    id: '1003',
+                    title: 'third note',
+                    text: 'this is the third note'
+                }
+            ]
+        }
     }
     
 
@@ -36,10 +37,18 @@ export class NotesApp extends React.Component {
                 <NotesList 
                     className={styles.list}
                     notes={this.state.notes}
+                    handleSelection={this._selectNote}
                     />
                 <NotesDetail className={styles.detail}/>
             </div>
         )
+    }
+
+    _selectNote = (id) => {
+        // Choose a note to show
+        this.setState({
+            selectedNote: id
+        })
     }
 }
 
--- src/NotesList.js
+++ src/NotesList.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import styles from './NotesList.module.css';
 
-function NotesListItem({text}) {
+function NotesListItem({text, handleClick, id}) {
     return (
-        <li>{text}</li>
+        <li>
+            <a 
+                href="#"
+                onClick={(e) => {
+                    e.preventDefault();
+                    console.log("You have clicked");
+                    handleClick(id);
+                }}
+                >
+                    {text}
+            </a>
+        </li>
     );
 }
 
-export default function NotesList({notes, className}) {
+export default function NotesList({notes, className, handleSelection}) {
   
-    const items = notes.map(note => <NotesListItem text={note.title}/>)
+    // const items = notes.map(({title}) => <NotesListItem text={title} handleClick={handleSelection}/>)
+    const items = notes.map(note => <NotesListItem id={note.id} text={note.title} handleClick={handleSelection}/>)
   
     return (
     <ul className={`${styles.list} ${className}`}>
